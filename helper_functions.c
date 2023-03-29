cef087de9602b4a73e533fdd2963b97ba09add86
PhantomMik3 <miguelforex77@gmail.com>
2023-03-29 20:52:24 +0000

    implemented edge case shortened Printf file by eliminating buffer and malloc, fixed helper functions

--- helper_functions.c
+++ helper_functions.c
@@ -4,84 +4,81 @@
  * func_char - function for printing a char
  * @_printf_: the argument list that has the character
  * @idx: pointer to character counter
- * @buffer: buffer to store char
  * Return: amount of characters printed
  */
 
-int func_char(va_list _printf_, char *buffer, int *idx)
+int func_char(va_list _printf_, int *idx)
 {
 	char c;
 
 	c = (char)va_arg(_printf_, int);
-	buffer[(*idx)++] = c;
+	write(1, &c, 1);
+	(*idx)++;
 	return (1);
 }
 
 /**
  * func_string - function for printing a string
  * @_printf_: the argument list that has the string
- * @buffer: buffer to store string
  * @idx: pointer to character counter
  * Return: string length
  */
 
-int func_string(va_list _printf_, char *buffer, int *idx)
+int func_string(va_list _printf_, int *idx)
 {
 	const char *s;
 	int len;
-
-
+	
 	s = va_arg(_printf_, char *);
+	if (s == NULL)
+		s = "(null)";
 	len = strlen(s);
-
+	
 	for (; *s != '\0'; s++)
 	{
-		buffer[(*idx)++] = *s;
-
+		write(1, s, 1);
 	}
+	(*idx)++;
 	return (len);
 }
 
 /**
  * func_percent - function for printing a %
  * @idx: pointer to character counter
- * @buffer: buffer to store percent sign
  * Return: 1
  */
 
-int func_percent(char *buffer, int *idx)
+int func_percent(int *idx)
 {
 	char percent = '%';
-
-	buffer[(*idx)++] = percent;
+	write(1, &percent, 1);
+	(*idx)++;
 	return (1);
 }
 
 /**
  * func_int - function for printing an integer
- * @buffer: buffer to store integer
  * @_printf_: list of arguments
  * @idx: pointer to arguments
  * Return: amount of numbers printed
  */
 
-int func_int(va_list _printf_, char *buffer, int *idx)
+int func_int(va_list _printf_, int *idx)
 {
 	int n = va_arg(_printf_, int);
 	int counter = 0;
-	unsigned int un;
-	unsigned int t;
-	unsigned int d = 1;
+	int t;
+	int d = 1;
 	char dig;
-
+	
 	if (n < 0)
 	{
-		buffer[(*idx)++] = '-';
+		write(1, "-", 1);
 		counter++;
 		n = -n;
 	}
-	un = (unsigned int)n;
-	t = un;
+	
+	t = n;
 
 	while (t >= 10)
 	{
@@ -91,12 +88,12 @@ int func_int(va_list _printf_, char *buffer, int *idx)
 
 	while (d > 0)
 	{
-		dig = (un / d) % 10 + '0';
-		buffer[(*idx)++] = dig;
+		dig = (n / d) % 10 + '0';
+		write(1, &dig, 1);
 		counter++;
-
 		d /= 10;
 	}
+	*idx += counter;
 
 	return (counter);
 }
